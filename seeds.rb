d14bb152c84b77e84f53e3daf8af76d8daab048f
José Aureliano <junior.rjsistemas@gmail.com>
2014-06-17 15:48:51 -0300

    Otimização de codigos

--- seeds.rb
+++ seeds.rb
@@ -1,314 +1,109 @@
 module BancosBrasileiros
 
   def self.populate
-    b = Banco.create(:codigo => '246', :banco => 'Banco ABC Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '356', :banco => 'Banco ABN AMRO Real S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '025', :banco => 'Banco Alfa S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '641', :banco => 'Banco Alvorada S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '029', :banco => 'Banco Banerj S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '038', :banco => 'Banco Banestado S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '740', :banco => 'Banco Barclays S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '107', :banco => 'Banco BBM S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '031', :banco => 'Banco Beg S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '036', :banco => 'Banco Bem S.A.')
-    b.save
-    
-    b = Banco.create(:codigo => '096', :banco => 'Banco BM&F de Serviços de Liquidação e Custódia S.A')
-    b.save
-
-    b = Banco.create(:codigo => '394', :banco => 'Banco BMC S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '318', :banco => 'Banco BMG S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '752', :banco => 'Banco BNP Paribas Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '248', :banco => 'Banco Boavista Interatlântico S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '237', :banco => 'Banco Bradesco S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '225', :banco => 'Banco Brascan S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '263', :banco => 'Banco Cacique S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '222', :banco => 'Banco Calyon Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '040', :banco => 'Banco Cargill S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '745', :banco => 'Banco Citibank S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '215', :banco => 'Banco Comercial e de Investimento Sudameris S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '756', :banco => 'Banco Cooperativo do Brasil S.A. – BANCOOB')
-    b.save
-
-    b = Banco.create(:codigo => '748', :banco => 'Banco Cooperativo Sicredi S.A. – BANSICREDI')
-    b.save
-
-    b = Banco.create(:codigo => '505', :banco => 'Banco Credit Suisse (Brasil) S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '229', :banco => 'Banco Cruzeiro do Sul S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '003', :banco => 'Banco da Amazônia S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '707', :banco => 'Banco Daycoval S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '024', :banco => 'Banco de Pernambuco S.A. – BANDEPE')
-    b.save
-
-    b = Banco.create(:codigo => '456', :banco => 'Banco de Tokyo-Mitsubishi UFJ Brasil S.A.')
-    b.save
-    
-    b = Banco.create(:codigo => '214', :banco => 'Banco Dibens S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '001', :banco => 'Banco do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '027', :banco => 'Banco do Estado de Santa Catarina S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '047', :banco => 'Banco do Estado de Sergipe S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '037', :banco => 'Banco do Estado do Pará S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '041', :banco => 'Banco do Estado do Rio Grande do Sul S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '004', :banco => 'Banco do Nordeste do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '265', :banco => 'Banco Fator S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '224', :banco => 'Banco Fibra S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '175', :banco => 'Banco Finasa S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '252', :banco => 'Banco Fininvest S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '233', :banco => 'Banco GE Capital S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '734', :banco => 'Banco Gerdau S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '612', :banco => 'Banco Guanabara S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '063', :banco => 'Banco Ibi S.A. Banco Múltiplo')
-    b.save
-
-    b = Banco.create(:codigo => '604', :banco => 'Banco Industrial do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '320', :banco => 'Banco Industrial e Comercial S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '653', :banco => 'Banco Indusval S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '630', :banco => 'Banco Intercap S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '249', :banco => 'Banco Investcred Unibanco S.A.')
-    b.save
-    
-    b = Banco.create(:codigo => '184-8', :banco => 'Banco Itaú BBA S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '652', :banco => 'Banco Itaú Holding Financeira S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '341', :banco => 'Banco Itaú S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '479', :banco => 'Banco ItaúBank S.A')
-    b.save
-
-    b = Banco.create(:codigo => '376', :banco => 'Banco J. P. Morgan S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '074', :banco => 'Banco J. Safra S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '600', :banco => 'Banco Luso Brasileiro S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '392', :banco => 'Banco Mercantil de São Paulo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '389', :banco => 'Banco Mercantil do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '755', :banco => 'Banco Merrill Lynch de Investimentos S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '151', :banco => 'Banco Nossa Caixa S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '045', :banco => 'Banco Opportunity S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '623', :banco => 'Banco Panamericano S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '611', :banco => 'Banco Paulista S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '643', :banco => 'Banco Pine S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '638', :banco => 'Banco Prosper S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '747', :banco => 'Banco Rabobank International Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '633', :banco => 'Banco Rendimento S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '072', :banco => 'Banco Rural Mais S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '453', :banco => 'Banco Rural S.A.')
-    b.save
-    
-    b = Banco.create(:codigo => '422', :banco => 'Banco Safra S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '008', :banco => 'Banco Santander Banespa S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '250', :banco => 'Banco Schahin S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '749', :banco => 'Banco Simples S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '366', :banco => 'Banco Société Générale Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '637', :banco => 'Banco Sofisa S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '347', :banco => 'Banco Sudameris Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '464', :banco => 'Banco Sumitomo Mitsui Brasileiro S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '634', :banco => 'Banco Triângulo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '208', :banco => 'Banco UBS Pactual S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '247', :banco => 'Banco UBS S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '116', :banco => 'Banco Único S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '655', :banco => 'Banco Votorantim S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '610', :banco => 'Banco VR S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '370', :banco => 'Banco WestLB do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '021', :banco => 'BANESTES S.A. Banco do Estado do Espírito Santo')
-    b.save
-
-    b = Banco.create(:codigo => '719', :banco => 'Banif-Banco Internacional do Funchal (Brasil)S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '204', :banco => 'Bankpar Banco Multiplo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '073-6', :banco => 'BB Banco Popular do Brasil S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '069-8', :banco => 'BPN Brasil Banco Mútiplo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '070', :banco => 'BRB – Banco de Brasília S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '104', :banco => 'Caixa Econômica Federal')
-    b.save
-
-    b = Banco.create(:codigo => '477', :banco => 'Citibank N.A.')
-    b.save
-
-    b = Banco.create(:codigo => '487', :banco => 'Deutsche Bank S.A. – Banco Alemão')
-    b.save
-
-    b = Banco.create(:codigo => '751', :banco => 'Dresdner Bank Brasil S.A. – Banco Múltiplo')
-    b.save
-
-    b = Banco.create(:codigo => '210', :banco => 'Dresdner Bank Lateinamerika Aktiengesellschaft')
-    b.save
-
-    b = Banco.create(:codigo => '062', :banco => 'Hipercard Banco Múltiplo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '399', :banco => 'HSBC Bank Brasil S.A. – Banco Múltiplo')
-    b.save
-
-    b = Banco.create(:codigo => '492', :banco => 'ING Bank N.V.')
-    b.save
-
-    b = Banco.create(:codigo => '488', :banco => 'JPMorgan Chase Bank')
-    b.save
-
-    b = Banco.create(:codigo => '065', :banco => 'Lemon Bank Banco Múltiplo S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '409', :banco => 'UNIBANCO – União de Bancos Brasileiros S.A.')
-    b.save
-
-    b = Banco.create(:codigo => '230', :banco => 'Unicard Banco Múltiplo S.A.')
-    b.save
+    Banco.create(:codigo => '246', :banco => 'Banco ABC Brasil S.A.')
+    Banco.create(:codigo => '356', :banco => 'Banco ABN AMRO Real S.A.')
+    Banco.create(:codigo => '025', :banco => 'Banco Alfa S.A.')
+    Banco.create(:codigo => '641', :banco => 'Banco Alvorada S.A.')
+    Banco.create(:codigo => '029', :banco => 'Banco Banerj S.A.')
+    Banco.create(:codigo => '038', :banco => 'Banco Banestado S.A.')
+    Banco.create(:codigo => '740', :banco => 'Banco Barclays S.A.')
+    Banco.create(:codigo => '107', :banco => 'Banco BBM S.A.')
+    Banco.create(:codigo => '031', :banco => 'Banco Beg S.A.')
+    Banco.create(:codigo => '036', :banco => 'Banco Bem S.A.')
+    Banco.create(:codigo => '096', :banco => 'Banco BM&F de Serviços de Liquidação e Custódia S.A')
+    Banco.create(:codigo => '394', :banco => 'Banco BMC S.A.')
+    Banco.create(:codigo => '318', :banco => 'Banco BMG S.A.')
+    Banco.create(:codigo => '752', :banco => 'Banco BNP Paribas Brasil S.A.')
+    Banco.create(:codigo => '248', :banco => 'Banco Boavista Interatlântico S.A.')
+    Banco.create(:codigo => '237', :banco => 'Banco Bradesco S.A.')
+    Banco.create(:codigo => '225', :banco => 'Banco Brascan S.A.')
+    Banco.create(:codigo => '263', :banco => 'Banco Cacique S.A.')
+    Banco.create(:codigo => '222', :banco => 'Banco Calyon Brasil S.A.')
+    Banco.create(:codigo => '040', :banco => 'Banco Cargill S.A.')
+    Banco.create(:codigo => '745', :banco => 'Banco Citibank S.A.')
+    Banco.create(:codigo => '215', :banco => 'Banco Comercial e de Investimento Sudameris S.A.')
+    Banco.create(:codigo => '756', :banco => 'Banco Cooperativo do Brasil S.A. – BANCOOB')
+    Banco.create(:codigo => '748', :banco => 'Banco Cooperativo Sicredi S.A. – BANSICREDI')
+    Banco.create(:codigo => '505', :banco => 'Banco Credit Suisse (Brasil) S.A.')
+    Banco.create(:codigo => '229', :banco => 'Banco Cruzeiro do Sul S.A.')
+    Banco.create(:codigo => '003', :banco => 'Banco da Amazônia S.A.')
+    Banco.create(:codigo => '707', :banco => 'Banco Daycoval S.A.')
+    Banco.create(:codigo => '024', :banco => 'Banco de Pernambuco S.A. – BANDEPE')
+    Banco.create(:codigo => '456', :banco => 'Banco de Tokyo-Mitsubishi UFJ Brasil S.A.')
+    Banco.create(:codigo => '214', :banco => 'Banco Dibens S.A.')
+    Banco.create(:codigo => '001', :banco => 'Banco do Brasil S.A.')
+    Banco.create(:codigo => '027', :banco => 'Banco do Estado de Santa Catarina S.A.')
+    Banco.create(:codigo => '047', :banco => 'Banco do Estado de Sergipe S.A.')
+    Banco.create(:codigo => '037', :banco => 'Banco do Estado do Pará S.A.')
+    Banco.create(:codigo => '041', :banco => 'Banco do Estado do Rio Grande do Sul S.A.')
+    Banco.create(:codigo => '004', :banco => 'Banco do Nordeste do Brasil S.A.')
+    Banco.create(:codigo => '265', :banco => 'Banco Fator S.A.')
+    Banco.create(:codigo => '224', :banco => 'Banco Fibra S.A.')
+    Banco.create(:codigo => '175', :banco => 'Banco Finasa S.A.')
+    Banco.create(:codigo => '252', :banco => 'Banco Fininvest S.A.')
+    Banco.create(:codigo => '233', :banco => 'Banco GE Capital S.A.')
+    Banco.create(:codigo => '734', :banco => 'Banco Gerdau S.A.')
+    Banco.create(:codigo => '612', :banco => 'Banco Guanabara S.A.')
+    Banco.create(:codigo => '063', :banco => 'Banco Ibi S.A. Banco Múltiplo')
+    Banco.create(:codigo => '604', :banco => 'Banco Industrial do Brasil S.A.')
+    Banco.create(:codigo => '320', :banco => 'Banco Industrial e Comercial S.A.')
+    Banco.create(:codigo => '653', :banco => 'Banco Indusval S.A.')
+    Banco.create(:codigo => '630', :banco => 'Banco Intercap S.A.')
+    Banco.create(:codigo => '249', :banco => 'Banco Investcred Unibanco S.A.')
+    Banco.create(:codigo => '184-8', :banco => 'Banco Itaú BBA S.A.')
+    Banco.create(:codigo => '652', :banco => 'Banco Itaú Holding Financeira S.A.')
+    Banco.create(:codigo => '341', :banco => 'Banco Itaú S.A.')
+    Banco.create(:codigo => '479', :banco => 'Banco ItaúBank S.A')
+    Banco.create(:codigo => '376', :banco => 'Banco J. P. Morgan S.A.')
+    Banco.create(:codigo => '074', :banco => 'Banco J. Safra S.A.')
+    Banco.create(:codigo => '600', :banco => 'Banco Luso Brasileiro S.A.')
+    Banco.create(:codigo => '392', :banco => 'Banco Mercantil de São Paulo S.A.')
+    Banco.create(:codigo => '389', :banco => 'Banco Mercantil do Brasil S.A.')
+    Banco.create(:codigo => '755', :banco => 'Banco Merrill Lynch de Investimentos S.A.')
+    Banco.create(:codigo => '151', :banco => 'Banco Nossa Caixa S.A.')
+    Banco.create(:codigo => '045', :banco => 'Banco Opportunity S.A.')
+    Banco.create(:codigo => '623', :banco => 'Banco Panamericano S.A.')
+    Banco.create(:codigo => '611', :banco => 'Banco Paulista S.A.')
+    Banco.create(:codigo => '643', :banco => 'Banco Pine S.A.')
+    Banco.create(:codigo => '638', :banco => 'Banco Prosper S.A.')
+    Banco.create(:codigo => '747', :banco => 'Banco Rabobank International Brasil S.A.')
+    Banco.create(:codigo => '633', :banco => 'Banco Rendimento S.A.')
+    Banco.create(:codigo => '072', :banco => 'Banco Rural Mais S.A.')
+    Banco.create(:codigo => '453', :banco => 'Banco Rural S.A.')
+    Banco.create(:codigo => '422', :banco => 'Banco Safra S.A.')
+    Banco.create(:codigo => '008', :banco => 'Banco Santander Banespa S.A.')
+    Banco.create(:codigo => '250', :banco => 'Banco Schahin S.A.')
+    Banco.create(:codigo => '749', :banco => 'Banco Simples S.A.')
+    Banco.create(:codigo => '366', :banco => 'Banco Société Générale Brasil S.A.')
+    Banco.create(:codigo => '637', :banco => 'Banco Sofisa S.A.')
+    Banco.create(:codigo => '347', :banco => 'Banco Sudameris Brasil S.A.')
+    Banco.create(:codigo => '464', :banco => 'Banco Sumitomo Mitsui Brasileiro S.A.')
+    Banco.create(:codigo => '634', :banco => 'Banco Triângulo S.A.')
+    Banco.create(:codigo => '208', :banco => 'Banco UBS Pactual S.A.')
+    Banco.create(:codigo => '247', :banco => 'Banco UBS S.A.')
+    Banco.create(:codigo => '116', :banco => 'Banco Único S.A.')
+    Banco.create(:codigo => '655', :banco => 'Banco Votorantim S.A.')
+    Banco.create(:codigo => '610', :banco => 'Banco VR S.A.')
+    Banco.create(:codigo => '370', :banco => 'Banco WestLB do Brasil S.A.')
+    Banco.create(:codigo => '021', :banco => 'BANESTES S.A. Banco do Estado do Espírito Santo')
+    Banco.create(:codigo => '719', :banco => 'Banif-Banco Internacional do Funchal (Brasil)S.A.')
+    Banco.create(:codigo => '204', :banco => 'Bankpar Banco Multiplo S.A.')
+    Banco.create(:codigo => '073-6', :banco => 'BB Banco Popular do Brasil S.A.')
+    Banco.create(:codigo => '069-8', :banco => 'BPN Brasil Banco Mútiplo S.A.')
+    Banco.create(:codigo => '070', :banco => 'BRB – Banco de Brasília S.A.')
+    Banco.create(:codigo => '104', :banco => 'Caixa Econômica Federal')
+    Banco.create(:codigo => '477', :banco => 'Citibank N.A.')
+    Banco.create(:codigo => '487', :banco => 'Deutsche Bank S.A. – Banco Alemão')
+    Banco.create(:codigo => '751', :banco => 'Dresdner Bank Brasil S.A. – Banco Múltiplo')
+    Banco.create(:codigo => '210', :banco => 'Dresdner Bank Lateinamerika Aktiengesellschaft')
+    Banco.create(:codigo => '062', :banco => 'Hipercard Banco Múltiplo S.A.')
+    Banco.create(:codigo => '399', :banco => 'HSBC Bank Brasil S.A. – Banco Múltiplo')
+    Banco.create(:codigo => '492', :banco => 'ING Bank N.V.')
+    Banco.create(:codigo => '488', :banco => 'JPMorgan Chase Bank')
+    Banco.create(:codigo => '065', :banco => 'Lemon Bank Banco Múltiplo S.A.')
+    Banco.create(:codigo => '409', :banco => 'UNIBANCO – União de Bancos Brasileiros S.A.')
+    Banco.create(:codigo => '230', :banco => 'Unicard Banco Múltiplo S.A.')
     
   end
   
